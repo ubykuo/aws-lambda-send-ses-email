1495a9166f134656f680b42427f14c24241b63e0
Sebastián Gallardo <seba.gallardo@ubykuo.com>
2017-08-27 21:41:51 -0300

    Deleted the S3 communication and replaced with the read of the template file on the templates directory.

---
+++ config.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+var config = {
+    "templateKey" : "templates/template.html",
+    "targetAddress" : "info@ubykuo.com",
+    "fromAddress": "Me <info@ubykuo.com>",
+    "defaultSubject" : "Email From {{name}}"
+}
+
+module.exports = config
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var aws = require('aws-sdk');
 
 var ses = new aws.SES();
-var s3 = new aws.S3();
+var fs = require('fs')
 
 exports.handler = function (event, context) {
     
@@ -17,37 +17,20 @@ exports.handler = function (event, context) {
 
     var config = require('./config.js');
     
-    // Make sure some expected results are present
     if (event.name == null) {
         event.name = event.email;
     }
     
-    // Make sure we have a subject.
-    // If the event didn't include it, then
-    // pull it from the configuration.
-    // If we still don't have a subject, then
-    // just make one up.
     if (event.subject == null) {
         event.subject = config.defaultSubject;
-        
-        if (event.subject == null) {
-            event.subject = "Mail from {{name}}";
-        }
     }
     
-    console.log('Loading template from ' + config.templateKey + ' in ' + config.templateBucket);
-
-    // Read the template file
-    s3.getObject({
-        Bucket: config.templateBucket, 
-        Key: config.templateKey
-    }, function (err, data) {
+    fs.readFile(config.templateKey, 'utf8', function (err,data) {
         if (err) {
-            // Error
             console.log(err, err.stack);
-            context.fail('Internal Error: Failed to load template from s3.')
-        } else {
-            var templateBody = data.Body.toString();
+            context.fail('Internal Error: Failed to load template.')
+        }else{
+            var templateBody = data.toString(); 
             console.log("Template Body: " + templateBody);
             
             // Convert newlines in the message
@@ -61,9 +44,6 @@ exports.handler = function (event, context) {
             // Perform the substitutions
             var mark = require('markup-js');
             
-            var subject = mark.up(event.subject, event);
-            console.log("Final subject: " + subject);
-            
             var message = mark.up(templateBody, event);
             console.log("Final message: " + message);
             
@@ -76,8 +56,14 @@ exports.handler = function (event, context) {
                 Message: {
                     
                     Subject: {
-                        Data: subject,
+                        Data: 'Email from ubykuo.com', // subject
                         Charset: 'UTF-8'
+                    }, 
+                    Body: {
+                        Html: {
+                            Data: message,
+                            Charset: 'UTF-8'
+                        }
                     }
                 },
                 Source: config.fromAddress,
@@ -86,33 +72,13 @@ exports.handler = function (event, context) {
                 ]
             };
             
-            var fileExtension = config.templateKey.split(".").pop();
-            if (fileExtension.toLowerCase() == 'html') {
-                params.Message.Body = {
-                    Html: {
-                        Data: message,
-                        Charset: 'UTF-8'
-                    }
-                };
-            } else if (fileExtension.toLowerCase() == 'txt') {
-                params.Message.Body = {
-                    Text: {
-                        Data: message,
-                        Charset: 'UTF-8'
-                    }
-                };
-            } else {
-                context.fail('Internal Error: Unrecognized template file extension: ' + fileExtension);
-                return;
-            }
-
             // Send the email
             ses.sendEmail(params, function (err, data) {
                 if (err) {
                     console.log(err, err.stack);
                     context.fail('Internal Error: The email could not be sent.');
                 } else {
-                    console.log(data);           // successful response
+                    console.log(data);
                     context.succeed('The email was successfully sent to ' + event.email);
                 }
             });
